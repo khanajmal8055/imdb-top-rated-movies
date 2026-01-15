95a7bb96bed19d9fb6bad1f61c1a6c0d3bfcc449
khanajmal8055 <mdajamal3364@gmail.com>
2026-01-15 20:10:25 +0530

    refactor: update package.json and src/index.js for improved structure and dependencies
    - Updated project description in package.json.
    - Changed main entry point to src/index.js.
    - Added start and dev scripts for easier execution.
    - Specified Node.js engine version requirement.
    - Moved dotenv and nodemon to devDependencies.
    - Downgraded express to version 4.18.3 for compatibility.
    - Simplified dotenv configuration in src/index.js by using import syntax.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import databaseConnection from "./DB/index.js";
-import dotenv from "dotenv";
+import "dotenv/config"
 
 import { app } from "./app.js";
 
-dotenv.config({
-    path:'/.env'
-})
+
 
 databaseConnection()
 .then(() => {
